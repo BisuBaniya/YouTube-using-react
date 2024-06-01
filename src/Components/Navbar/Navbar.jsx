e55092461edd93fd57eff33dae0deed19e615f88
BisuBaniya <bisusiddharth1806@gmail.com>
2024-06-01 14:16:32 +0530

    Commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,12 +8,12 @@ import more_icon from '../../assets/more.png'
 import notification_icon from '../../assets/notification.png'
 import profile_icon from '../../assets/jack.png'
 
-const Navbar = () => {
+const Navbar = ({setSidebar}) => {
   return (
     <nav className="flex-div">
 
         <div className="nav-left flex-div">
-            <img src={menu_icon} className='menu_icon' alt="" />
+            <img src={menu_icon} className='menu_icon' onClick={()=>setSidebar(prev=>prev===false?true:false)} alt="" />
             <img src={logo} className='logo' alt="" />
         </div>
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -16,10 +16,10 @@ import megan from '../../assets/megan.png'
 import cameron from '../../assets/cameron.png'
 
 
-const Sidebar = () => {
+const Sidebar = ({sidebar}) => {
   return (
     <>
-    <div className="sidebar">
+    <div className={`sidebar ${sidebar?"":'small-sidebar'}`}>
       <div className="shortcut-links">
 
         <div className="side-link">
@@ -52,6 +52,28 @@ const Sidebar = () => {
 
         <hr/>
         </div>
+
+        <div className="subscribed-list">
+          <h3>Subscribed</h3>
+
+          <div className="side-link">
+            <img src={jack} alt="" /><p>PewDiePie</p>
+          </div>
+          <div className="side-link">
+            <img src={simon} alt="" /><p>MrBeast</p>
+          </div>
+          <div className="side-link">
+            <img src={tom} alt="" /><p>Justin Bieber</p>
+          </div>
+          <div className="side-link">
+            <img src={megan} alt="" /><p>5-Minute Crafts</p>
+          </div>
+          <div className="side-link">
+            <img src={cameron} alt="" /><p>Nas Daily</p>
+          </div>
+
+        </div>
+
         </div> 
     </>
   )
