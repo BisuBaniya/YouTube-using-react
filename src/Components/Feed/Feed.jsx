c3a295951636265677ac00803df5680b07305ecf
BisuBaniya <bisusiddharth1806@gmail.com>
2024-06-04 10:06:52 +0530

    Commit

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,42 +1,39 @@
-import React from 'react'
-import './Feed.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
-import { Link } from 'react-router-dom'
-import { API_KEY } from '../../data'
-import {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import './Feed.css';
+import { Link } from 'react-router-dom';
+import { API_KEY } from '../../data';
 
-const Feet = (category) => {
-  const [data,setData] = useState([]);
-  const fetchData = async ()=>{
-    const videoList_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2contentDetails%2statistics%chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
-    await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
-  }
+const Feed = ({ category }) => {
+  const [data, setData] = useState([]);
 
-  useEffect(()=>{
+  const fetchData = async () => {
+    const videoList_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+    
+    try {
+      const response = await fetch(videoList_url);
+      const result = await response.json();
+      setData(result.items);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
-  },[category])
+  }, [category]);
 
   return (
-    <>
-      <div className="feed">
-        <Link to={'video/20/4521'} className="card">
-          <img src={thumbnail1} alt="" />
-          <h2>Best channel to learn coding that help you to be a web developer</h2>
-          <h3>GreatStack</h3>
-          <p>15k views &bull; 2 days ago</p>
+    <div className="feed">
+      {data.map((item, index) => (
+        <Link key={item.id} to={`/video/${item.snippet.categoryId}/${item.id}`} className="card">
+          <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title} />
+          <h2>{item.snippet.title}</h2>
+          <h3>{item.snippet.channelTitle}</h3>
+          <p>{item.statistics.viewCount} views &bull; {new Date(item.snippet.publishedAt).toLocaleDateString()}</p>
         </Link>
-      </div>
-    </>
-  )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Feet
-
-
+export default Feed;
